ed77d8ad8127da2a4e87f37e0b473974ab7cc566
Panigrahi, Akshaya <apanigra@itron.com>
2025-08-14 00:34:13 +0530

    Test-Commit

--- obj/Debug/net6.0/HeadEndDynamicXpath.AssemblyInfo.cs
+++ obj/Debug/net6.0/HeadEndDynamicXpath.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HeadEndDynamicXpath")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+55757b8bf003aa9324d1ee827e0c8eee18d5a46b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98dc21ebc2b037aeb0a310e388110e1d4c4f1f86")]
 [assembly: System.Reflection.AssemblyProductAttribute("HeadEndDynamicXpath")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HeadEndDynamicXpath")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
